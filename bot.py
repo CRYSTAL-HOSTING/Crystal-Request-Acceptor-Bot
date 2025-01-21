5e160afc0b2f8258bd9b7bb3ae302dca1000a5a3
CRYSTAL HOSTING <crystal.hq.hosting@gmail.com>
2025-01-22 00:36:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,12 @@
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
 from pyrogram import Client, filters
 from pyrogram.types import *
-from motor.motor_asyncio import AsyncIOMotorClient  
+from motor.motor_asyncio import AsyncIOMotorClient
 from os import environ as env
 import asyncio, datetime, time
 
-
 ACCEPTED_TEXT = "Hey {user}\n\nYour Request For {chat} Is Accepted ✅"
-START_TEXT = "Hai {}\n\nI am Auto Request Accept Bot With Working For All Channel. Add Me In Your Channel To Use"
+START_TEXT = "Hai {}\n\nI am Auto Request Accept Bot Working For All Channels. Add Me In Your Channel To Use."
 
 API_ID = int(env.get('API_ID'))
 API_HASH = env.get('API_HASH')
@@ -15,25 +14,31 @@ BOT_TOKEN = env.get('BOT_TOKEN')
 DB_URL = env.get('DB_URL')
 ADMINS = int(env.get('ADMINS'))
 
+# MongoDB connection
 Dbclient = AsyncIOMotorClient(DB_URL)
 Cluster = Dbclient['Cluster0']
 Data = Cluster['users']
-Bot = Client(name='AutoAcceptBot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-       
-      
-     
-@Bot.on_message(filters.command("start") & filters.private)                    
+
+# Pyrogram client
+Bot = Client(
+    name='AutoAcceptBot',
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+)
+
+@Bot.on_message(filters.command("start") & filters.private)
 async def start_handler(c, m):
     user_id = m.from_user.id
-    if not await Data.find_one({'id': user_id}): await Data.insert_one({'id': user_id})
+    if not await Data.find_one({'id': user_id}):
+        await Data.insert_one({'id': user_id})
     button = [[        
         InlineKeyboardButton('Updates', url='https://t.me/mkn_bots_updates'),
         InlineKeyboardButton('Support', url='https://t.me/MKN_BOTZ_DISCUSSION_GROUP')
     ]]
     return await m.reply_text(text=START_TEXT.format(m.from_user.mention), disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(button))
-          
 
-@Bot.on_message(filters.command(["broadcast", "users"]) & filters.user(ADMINS))  
+@Bot.on_message(filters.command(["broadcast", "users"]) & filters.user(ADMINS))
 async def broadcast(c, m):
     if m.text == "/users":
         total_users = await Data.count_documents({})
@@ -67,25 +72,21 @@ async def broadcast(c, m):
             failed += 1
         done += 1
         if not done % 20:
-            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nFailed: {failed}")    
-    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nFailed: {failed}")
+    time_taken = datetime.timedelta(seconds=int(time.time() - start_time))
     await sts.delete()
-    await message.reply_text(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nFailed: {failed}", quote=True)
+    await m.reply_text(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nFailed: {failed}", quote=True)
 
-  
- 
 @Bot.on_chat_join_request()
 async def req_accept(c, m):
     user_id = m.from_user.id
     chat_id = m.chat.id
-    if not await Data.find_one({'id': user_id}): await Data.insert_one({'id': user_id})
+    if not await Data.find_one({'id': user_id}):
+        await Data.insert_one({'id': user_id})
     await c.approve_chat_join_request(chat_id, user_id)
-    try: await c.send_message(user_id, ACCEPTED_TEXT.format(user=m.from_user.mention, chat=m.chat.title))
-    except Exception as e: print(e)
-   
-   
+    try:
+        await c.send_message(user_id, ACCEPTED_TEXT.format(user=m.from_user.mention, chat=m.chat.title))
+    except Exception as e:
+        print(e)
 
 Bot.run()
-
-
-
